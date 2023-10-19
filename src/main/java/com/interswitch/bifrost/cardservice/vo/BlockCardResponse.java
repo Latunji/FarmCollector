fdef0c3df920a0043b05c84bd941396d8694bd4f
Taiwo.Kasumu <taiwo.kasumu@wayapaychat.com>
2023-10-19 13:38:43 +0100

    - PTMFB Card Implementation

---
+++ src/main/java/com/interswitch/bifrost/cardservice/vo/BlockCardResponse.java
@@ -0,0 +1,11 @@
+package com.interswitch.bifrost.cardservice.vo;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class BlockCardResponse {
+    private boolean IsSuccessful;
+    private String responseMessage;
+}
---
+++ src/main/java/com/interswitch/bifrost/cardservice/vo/HotlistResponse.java
@@ -0,0 +1,15 @@
+package com.interswitch.bifrost.cardservice.vo;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class HotlistResponse {
+    private boolean IsSuccessful;
+    private String responseMessage;
+    private String batchNo;
+    private String identifier;
+    private String serialNo;
+    private String transactionReference;
+}
